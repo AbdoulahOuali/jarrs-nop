df75119fe27f38edd5354ade089761fda8a708f2
Abdoulah Ouali <32765173+MocroTronics@users.noreply.github.com>
2019-09-20 09:30:14 +0200

    Logging Post call and created retrofit classes, reformatted json nfc message

--- app/src/main/java/com/ingenico/epayments/jarrs/hackathon/jarrs_nop/ui/NFCSenderActivity.java
+++ app/src/main/java/com/ingenico/epayments/jarrs/hackathon/jarrs_nop/ui/NFCSenderActivity.java
@@ -15,10 +15,19 @@ import com.google.gson.Gson;
 import com.ingenico.epayments.jarrs.hackathon.jarrs_nop.R;
 import com.ingenico.epayments.jarrs.hackathon.jarrs_nop.nfc.OutcomingNfcManager;
 import com.ingenico.epayments.jarrs.hackathon.jarrs_nop.nfc.bean.NfcSenderMessage;
+import com.ingenico.epayments.jarrs.hackathon.jarrs_nop.rest.ApiService;
+import com.ingenico.epayments.jarrs.hackathon.jarrs_nop.rest.ServiceGenerator;
+import com.ingenico.epayments.jarrs.hackathon.jarrs_nop.rest.bean.Transfer;
 import com.ingenico.epayments.jarrs.hackathon.jarrs_nop.util.CustomDateFormatter;
+import com.ingenico.epayments.jarrs.hackathon.jarrs_nop.util.InternetConnection;
 import com.ingenico.epayments.jarrs.hackathon.jarrs_nop.util.MyProperties;
 
+import java.math.BigDecimal;
 import java.util.UUID;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class NFCSenderActivity extends AppCompatActivity implements OutcomingNfcManager.NfcActivity {
 
@@ -82,6 +91,7 @@ public class NFCSenderActivity extends AppCompatActivity implements OutcomingNfc
         Gson gson = new Gson();
         String outMessage = gson.toJson(nfcSenderMessage);
         Log.e(TAG, "sending message: " + outMessage);
+        updateSingleTransactionOnServer(nfcSenderMessage.getSender(), nfcSenderMessage.getAmount());
         this.tvOutcomingMessage.setText(outMessage);
         Log.e(TAG, "Message sent: " + outMessage);
     }
@@ -98,5 +108,49 @@ public class NFCSenderActivity extends AppCompatActivity implements OutcomingNfc
         // cause onNdefPushComplete is called from the Binder thread
         runOnUiThread(() ->
                 Toast.makeText(NFCSenderActivity.this, R.string.message_beaming_complete, Toast.LENGTH_SHORT).show());
+    }
+
+
+    private void updateSingleTransactionOnServer(String userId, String amount) {
+
+        String receiver = (userId == "sandip") ? "abdoulah" : "sandip";
+
+        Transfer transfer = Transfer.builder()
+                .uuid(UUID.randomUUID().toString())
+                .fromUserId(userId)
+                .toUserId(receiver)
+                .amount(BigDecimal.valueOf(Double.valueOf(amount)))
+                .currency("EUR")
+                .transactionTime(CustomDateFormatter.getCurrentTime())
+                .build();
+        Log.e(TAG, "created transaction: " + transfer.toString());
+
+        ApiService apiService = ServiceGenerator.createService(ApiService.class);
+        Log.e(TAG, "Start calling method updateSingleTransactionOnServer");
+
+        if (InternetConnection.checkConnection(getApplicationContext())) {
+            Log.e(TAG, "Good internet connection");
+        } else {
+            Log.e(TAG, "no internet connection");
+        }
+
+        Call<Transfer> call = apiService.updateSingleTransaction(transfer.getFromUserId(), transfer);
+        call.enqueue(new Callback<Transfer>() {
+            @Override
+            public void onResponse(Call<Transfer> call, Response<Transfer> response) {
+                if (!response.isSuccessful()) {
+                    Log.e(TAG, "http status code: " + response.code());
+                    Log.e(TAG, "http error: " + response.errorBody());
+                    return;
+                }
+                Log.e(TAG, response.message() + ", posted to server: " + response.raw());
+
+            }
+
+            @Override
+            public void onFailure(Call<Transfer> call, Throwable t) {
+                Log.e(TAG, t.getMessage());
+            }
+        });
     }
 }
